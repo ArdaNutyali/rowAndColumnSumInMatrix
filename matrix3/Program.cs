a30ad8ec04217557c0dfabf0a4e0dc56bf84cd5a
ArdaNutyali <64331573+ArdaNutyali@users.noreply.github.com>
2022-05-01 02:52:53 +0300

    Proje dosyası ekle.

---
+++ matrix3/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace matrix3
+{
+    internal class Program
+    {
+        static int[,] matrix = new int[5, 3];
+        static int[] sum1 = new int[5];
+        static int[] sum2 = new int[3];
+        static void sumRow()
+        {
+            for (int i = 0; i < 5; i++)
+            {
+                for (int j = 0; j < 3; j++)
+                {
+                    sum1[i] = sum1[i] + matrix[i, j];
+                }
+            }
+        }
+        static void sumColumn()
+        {
+            for (int i = 0; i < 5; i++)
+            {
+                for (int j = 0; j < 3; j++)
+                {
+                    sum2[j] = sum2[j] + matrix[i, j];
+                }
+            }
+        }
+        static void Main(string[] args)
+        {
+            Random rnd = new Random();
+            for (int i = 0; i < 5; i++)
+            {
+                for (int j = 0; j < 3; j++)
+                {
+                    matrix[i, j] = rnd.Next(1, 100);
+                    Console.Write(matrix[i, j] + " ");
+                }
+                Console.WriteLine();
+            }
+            Console.WriteLine();
+            sumColumn();
+            sumRow();
+            for (int i = 0; i < 5; i++)
+            {
+                Console.Write("{0}. Row Sum {1}\n", i + 1, sum1[i]);
+            }
+            Console.WriteLine();
+            for (int i = 0; i < 3; i++)
+            {
+                Console.Write("{0}. Column Sum: {1}\n", i + 1, sum2[i]);
+            }
+            Console.ReadKey();
+        }
+    }
+}
---
+++ matrix3/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Bir bütünleştirilmiş koda ilişkin Genel Bilgiler aşağıdaki öznitelikler kümesiyle
+// denetlenir. Bütünleştirilmiş kod ile ilişkili bilgileri değiştirmek için
+// bu öznitelik değerlerini değiştirin.
+[assembly: AssemblyTitle("matrix3")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("matrix3")]
+[assembly: AssemblyCopyright("Copyright ©  2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// ComVisible özniteliğinin false olarak ayarlanması bu bütünleştirilmiş koddaki türleri
+// COM bileşenleri için görünmez yapar. Bu bütünleştirilmiş koddaki bir türe
+// erişmeniz gerekirse ComVisible özniteliğini o türde true olarak ayarlayın.
+[assembly: ComVisible(false)]
+
+// Bu proje COM'un kullanımına sunulursa, aşağıdaki GUID tür kitaplığının kimliği içindir
+[assembly: Guid("964ef123-86a9-4f76-b219-50d5511f90b2")]
+
+// Bir derlemenin sürüm bilgileri aşağıdaki dört değerden oluşur:
+//
+//      Ana Sürüm
+//      İkincil Sürüm 
+//      Yapı Numarası
+//      Düzeltme
+//
+// Tüm değerleri belirtebilir veya varsayılan Derleme ve Düzeltme Numaralarını kullanmak için
+// aşağıda gösterildiği gibi '*' kullanabilirsiniz:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
